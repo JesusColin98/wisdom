fcdf7cc4ef17b68e545847e6f92b259a646028f1
JesusColin98 <jealcovi98@gmail.com>
2026-05-14 14:57:07 -0600

    feat: add script to install protocol buffer dependencies and generate Go source files

---
+++ wisdom/test.sh
@@ -0,0 +1,6 @@
+set -e
+apt-get update -qq && apt-get install -y -qq protobuf-compiler
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+export PATH=$PATH:$(go env GOPATH)/bin
+protoc --go_out=. --go_opt=module=github.com/google/wisdom --go-grpc_out=. --go-grpc_opt=module=github.com/google/wisdom proto/cortex.proto proto/thalamus.proto proto/mastery.proto proto/researcher.proto proto/curriculum.proto proto/integrations.proto proto/entity.proto proto/pubsub_events.proto
